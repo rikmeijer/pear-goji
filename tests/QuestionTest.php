530762a7dccaf1bb4f884602c87a70c9edb02745
Rik Meijer <rik@rikmeijer.nl>
2020-05-28 12:11:13 +0200

    use Question::ask to create Questions
    Signed-off-by: Rik Meijer <rik@rikmeijer.nl>

--- tests/QuestionTest.php
+++ tests/QuestionTest.php
@@ -15,7 +15,7 @@ class QuestionTest extends TestCase
 
     public function testQuestionCanBeAnswered()
     {
-        $question = new Question("How many roads must a man walk down, before you can call him a man?");
+        $question = Question::ask("How many roads must a man walk down, before you can call him a man?");
 
         $answer = $question->answer('5');
 
