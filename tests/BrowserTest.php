da34a74276347002c504682ab67715a083cf12fd
Rik Meijer <rik@rikmeijer.nl>
2020-06-20 16:30:31 +0200

    move java binary path to .env, in case another version than the system default is to be used
    Signed-off-by: Rik Meijer <rik@rikmeijer.nl>

--- tests/BrowserTest.php
+++ tests/BrowserTest.php
@@ -19,7 +19,7 @@ abstract class BrowserTest extends Selenium2TestCase
         self::$httpd->start(dirname(__DIR__));
 
 
-        self::$selenium = proc_open('java -jar ' . $_ENV['SELENIUM_JAR'], array(
+        self::$selenium = proc_open($_ENV['JAVA_BIN'] . ' -jar ' . $_ENV['SELENIUM_JAR'], array(
             0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
             1 => array("pipe", sys_get_temp_dir() . DIRECTORY_SEPARATOR . "selenium-stdout.txt", "a"),  // stdout is a pipe that the child will write to
             2 => array("file", sys_get_temp_dir() . DIRECTORY_SEPARATOR . "selenium-error-output.txt", "a") // stderr is a file to write to
