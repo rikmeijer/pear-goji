86b414dc38640d6f36773bc3490f9fde2e1f20b3
Rik Meijer <rik@rikmeijer.nl>
2020-06-25 15:22:02 +0200

    replace tests with codeception testing framework
    Signed-off-by: Rik Meijer <rik@rikmeijer.nl>

--- tests/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php declare(strict_types=1);
-
-require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
