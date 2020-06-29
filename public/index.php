44c5d126f44375cef961f3e4c0e09e996ddf4693
Rik Meijer <rik@rikmeijer.nl>
2020-06-29 12:31:34 +0200

    refactor to use twig to load templates and render them
    Signed-off-by: Rik Meijer <rik@rikmeijer.nl>

--- public/index.php
+++ public/index.php
@@ -1,31 +1,21 @@
-<?php /** @noinspection GlobalVariableUsageInspection */ declare(strict_types=1);
+<?php /** @noinspection GlobalVariableUsageInspection */
+declare(strict_types=1);
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+$loader = new FilesystemLoader(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'templates');
+$twig = new Environment($loader);
+
 if (array_key_exists('HTTP_ACCEPT', $_SERVER) && strpos($_SERVER['HTTP_ACCEPT'], 'application/json') !== false) {
     header('Content-Type: application/json');
     exit('{"status":"ok"}');
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    ?><!doctype html>
-<html lang="en">
-<head>
-    <title>𓀁</title>
-</head>
-<body>
-<h1>Answers</h1>
-</body>
-</html>
-    <?php
-    exit;
+    echo $twig->render('answers.html', []);
+} else {
+    echo $twig->render('index.html', []);
 }
-?><!doctype html>
-<html lang="en">
-<head>
-    <title>𓀁</title>
-</head>
-<body>
-<h1>𓀁</h1>
-<form action="/answers" method="post">
-    <label for="query"><input type="text" id="query" placeholder="Stel een vraag"></label>
-</form>
-</body>
-</html>
